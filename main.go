661acae1d786c203a28321c3a4c92ddd93072f4d
suchanya <suchanya.n@arise.tech>
2024-05-09 19:48:06 +0700

    feat: add crud with gorm

---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+const (
+	host     = "0.0.0.0"    // or the Docker service name if running in another container
+	port     = 5432         // default PostgreSQL port
+	user     = "myuser"     // as defined in docker-compose.yml
+	password = "mypassword" // as defined in docker-compose.yml
+	dbname   = "mydatabase" // as defined in docker-compose.yml
+)
+
+func main() {
+	// connect database
+	dsn := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold: time.Second, // Slow SQL threshold
+			LogLevel:      logger.Info, // Log level
+			Colorful:      true,        // Enable color
+		},
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{Logger: newLogger})
+	if err != nil {
+		// break or kill process
+		panic("failed to connect to database")
+	}
+	// compare table in database and struct in project
+	db.AutoMigrate(&User{})
+	fmt.Println("Database migration completed!")
+
+	// Create User
+	// newUser := &User{Username: "jane.smith", FirstName: "Jane", LastName: "Smith", Tier: 2}
+	// createUser(db, newUser)
+
+	// Get All Users
+	// users := getUserById(db, 2)
+	// formattedJson, err := json.MarshalIndent(users, "", "  ")
+	// if err != nil {
+	// 	fmt.Println("Error formatting JSON:", err)
+	// 	return
+	// }
+	// fmt.Println(string(formattedJson))
+
+	// Update User
+	// users.Username = "nature_lover"
+	// updateUser(db, users)
+
+	// Delete User
+	// deleteUser(db, 1)
+
+	// Search user by username
+	userByUsername := searchUser(db, "Jane")
+	formattedJson, err := json.MarshalIndent(userByUsername, "", "  ")
+	if err != nil {
+		fmt.Println("Error formatting JSON:", err)
+		return
+	}
+	fmt.Println(string(formattedJson))
+
+}
---
+++ models.go
@@ -0,0 +1,64 @@
+// คำสั่งจัดการ database
+// structure ในการสร้าง database
+
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Username  string `json:"username"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Tier      uint   `json:"tier"`
+}
+
+func getUserById(db *gorm.DB, id int) *User {
+	var user User
+	result := db.First(&user, id)
+	if result.Error != nil {
+		log.Fatalf("Error creating user: %v", result.Error)
+	}
+	return &user
+}
+
+func createUser(db *gorm.DB, user *User) {
+	result := db.Create(user)
+	if result.Error != nil {
+		log.Fatalf("Error creating user: %v", result.Error)
+	}
+	fmt.Printf("User created successfully")
+}
+
+func updateUser(db *gorm.DB, user *User) {
+	result := db.Save(&user)
+	if result.Error != nil {
+		log.Fatalf("Error updating user: %v", result.Error)
+	}
+	fmt.Printf("Update user successfully")
+}
+
+func deleteUser(db *gorm.DB, id int) {
+	var user User
+	// don't soft deletes the user
+	// result := db.Unscoped().Delete(&user, id)
+	result := db.Delete(&user, id)
+	if result.Error != nil {
+		log.Fatalf("Error deleting user: %v", result.Error)
+	}
+	fmt.Printf("Delete user successfully")
+}
+
+func searchUserByFirstName(db *gorm.DB, firstName string) []User {
+	var user []User
+	result := db.Where("first_name = ?", firstName).Find(&user)
+	if result.Error != nil {
+		log.Fatalf("Error seaching user: %v", result.Error)
+	}
+	return user
+}
